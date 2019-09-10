82643bb02e2596faa6429e085573151b7cbbdef6
clody01 <tseoue01@gmail.com>
2019-09-10 18:26:18 +0000

    Design of our application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EgressIncomeComponent } from './egress-income/egress-income.component';
+import { StatisticComponent } from './egress-income/statistic/statistic.component';
+import { DetailComponent } from './egress-income/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    EgressIncomeComponent,
+    StatisticComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/egress-income/egress-income.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-egress-income',
+  templateUrl: './egress-income.component.html',
+  styles: []
+})
+export class EgressIncomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
