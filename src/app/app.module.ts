0a1997bfc476387956f1aea7d2bfd99f3f0d1715
clody01 <tseoue01@gmail.com>
2019-09-16 23:45:33 +0000

    Global state of the application - ActionReducerMap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { EgressIncomeComponent } from './egress-income/egress-income.component';
-import { StatisticComponent } from './egress-income/statistic/statistic.component';
-import { DetailComponent } from './egress-income/detail/detail.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {EgressIncomeComponent} from './egress-income/egress-income.component';
+import {StatisticComponent} from './egress-income/statistic/statistic.component';
+import {DetailComponent} from './egress-income/detail/detail.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {SidebarComponent} from './shared/sidebar/sidebar.component';
 
 // Modules
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule} from '@angular/forms';
 
 // Firebase
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
+// NGRX
+import {StoreModule} from '@ngrx/store';
+import {appReducers} from './app.reducer';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 // Environment
 import {environment} from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -43,9 +49,15 @@ import {environment} from '../environments/environment';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
