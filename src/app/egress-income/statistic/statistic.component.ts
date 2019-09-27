72e651535151195994e02dfb604523ca26d7224c
clody01 <tseoue01@gmail.com>
2019-09-27 03:46:12 +0200

    Dashboard - Basic Statistics

--- src/app/egress-income/statistic/statistic.component.ts
+++ src/app/egress-income/statistic/statistic.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../app.reducer';
+import {Subscription} from 'rxjs';
+import {EgressIncome} from '../egress-income.model';
 
 @Component({
   selector: 'app-statistic',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class StatisticComponent implements OnInit {
+  incomes: number;
+  expenses: number;
+  howManyExpenses: number;
+  howManyIncomes: number;
+  statisticsSubscription: Subscription = new Subscription();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private  store: Store<AppState>) {
   }
 
+  ngOnInit() {
+    this.statisticsSubscription = this.store.select('egressIncome').subscribe(egressIncome => {
+      this.countIncome(egressIncome.items);
+    });
+  }
+
+  countIncome(items: EgressIncome[]) {
+    this.incomes = 0;
+    this.expenses = 0;
+    this.howManyExpenses = 0;
+    this.howManyIncomes = 0;
+    items.forEach(item => {
+      if (item.type === 'income') {
+        this.howManyIncomes++;
+        this.incomes += item.amount;
+      } else {
+        this.howManyExpenses++;
+        this.expenses += item.amount;
+      }
+    });
+  }
 }
