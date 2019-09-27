6876cf1dbf5df731062922d8ab9b66adf1ac4b31
clody01 <tseoue01@gmail.com>
2019-09-27 17:19:54 +0200

    Enter the name of the active user

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from '../../auth/auth.service';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../app.reducer';
+import {Subscription} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: []
 })
-export class SidebarComponent implements OnInit {
-
-  constructor(public authService: AuthService) { }
+export class SidebarComponent implements OnInit, OnDestroy {
+  currentUserName: string;
+  currentUserSubscription: Subscription = new Subscription();
+  constructor(public authService: AuthService, private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.currentUserSubscription = this.store.select('auth')
+      .pipe(
+        filter(auth => auth.user != null)
+      )
+      .subscribe(auth => this.currentUserName = auth.user.name);
   }
-
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
+  }
   logout() {
     this.authService.logout();
   }
